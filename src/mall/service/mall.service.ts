5a9dcc281402b0e107412f18d8d2d5f35834b10e
박소희 <shpark-personal>
2024-04-04 20:18:14 +0900

    feat : service 구조 구현

--- src/mall/service/mall.service.ts
+++ src/mall/service/mall.service.ts
@@ -1,4 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common'
+import {
+  PointResult,
+  SimpleResult,
+  ProductsResult,
+  ProductResult,
+} from '../models/Result'
+import { ProductItem } from '../models/Product'
+import { Errorcode } from '../models/Enums'
 
 @Injectable()
-export class MallService {}
+export class MallService {
+  charge(userId: string, amount: any): PointResult {
+    return { errorcode: Errorcode.Success, point: 0 }
+  }
+
+  point(userId: string): PointResult {
+    return { errorcode: Errorcode.Success, point: 0 }
+  }
+
+  getDetail(productId: number): ProductResult {
+    const p = {
+      id: productId,
+      name: '달력',
+      quantity: 3,
+    }
+    return { errorcode: Errorcode.Success, product: p }
+  }
+
+  order(userId: string, products: ProductItem[]): SimpleResult {
+    return { errorcode: Errorcode.Success }
+  }
+
+  pay(userId: string, products: ProductItem[]): SimpleResult {
+    return { errorcode: Errorcode.Success }
+  }
+
+  getRankedProducts(): ProductsResult {
+    return { errorcode: Errorcode.Success, products: [] }
+  }
+}
