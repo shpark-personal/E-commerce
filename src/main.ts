8025fcf38e8d2cc019878b78d6c12d997a7b335a
박소희 <shpark-personal>
2024-04-08 13:06:52 +0900

    init : swaggermodule setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const options = new DocumentBuilder()
+    .setTitle('Mall Server API')
+    .setDescription('Mall Server API description')
+    .setVersion('0.1')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api-docs', app, document)
   await app.listen(3000)
 }
 bootstrap()
