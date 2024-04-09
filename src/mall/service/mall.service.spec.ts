8c9fa09e692e50a74ee4efded333ba8da42080d7
박소희 <shpark-personal>
2024-04-09 15:33:09 +0900

    test : user repository test code 추가

--- src/mall/service/mall.service.spec.ts
+++ src/mall/service/mall.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { MallService } from './mall.service'
+import { IUSER_REPOSITORY } from '../repository/mall.interface'
+import { TestRepository } from '../repository/test.repository'
+import { Errorcode } from '../models/Enums'
 
 describe('MallService', () => {
   let service: MallService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MallService],
+      providers: [
+        MallService,
+        {
+          provide: IUSER_REPOSITORY,
+          useClass: TestRepository,
+        },
+      ],
     }).compile()
 
     service = module.get<MallService>(MallService)
@@ -14,5 +23,19 @@ describe('MallService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('charge point', () => {
+    it('success to charge point', () => {
+      const result = service.charge('userA', 10)
+      expect(result).toEqual({ errorcode: Errorcode.Success, point: 10 })
+    })
+  })
+
+  describe('get point', () => {
+    it('success to get point', () => {
+      const result = service.point('userB')
+      expect(result).toEqual({ errorcode: Errorcode.Success, point: 0 })
+    })
   })
 })
