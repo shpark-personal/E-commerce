1ec88309240d1439c92478e8ea998f94e6778084
박소희 <shpark-personal>
2024-04-17 17:30:20 +0900

    test : user repository 추가

---
+++ src/mall/repository/user.repository.spec.ts
@@ -0,0 +1,22 @@
+import { Errorcode } from '../models/Enums'
+import { UserRepository } from './user.repository'
+
+describe('userRepositoryTest', () => {
+  const repository = new UserRepository({
+    save: jest.fn(),
+    findOne: jest.fn(),
+  } as unknown as ConstructorParameters<typeof UserRepository>[0])
+
+  it('charge', () => {
+    const user = {
+      id: 'userA',
+      point: 10,
+    }
+    jest.spyOn(repository['users'], 'findOne').mockResolvedValue(user)
+
+    expect(repository.charge('userA', 10)).toEqual({
+      errorcode: Errorcode.Success,
+      point: user.point,
+    })
+  })
+})
