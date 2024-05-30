67789f1c7a641a445ccc310502e0d51e5e856788
박소희 <shpark-personal>
2024-05-30 15:36:16 +0900

    test : e2e 모듈 정리

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,47 @@
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
 import { Test, TestingModule } from '@nestjs/testing'
+import { UserService } from '../src/mall/service/user.service'
+import {
+  PointResult,
+  ProductDetailResult,
+  ProductsResult,
+  SimpleResult,
+} from '../src/mall/models/Result'
+import { ProductService } from '../src/mall/service/product.service'
+import { OrderService } from '../src/mall/service/order.service'
+import { PayService } from '../src/mall/service/pay.service'
 
 describe('AppController (e2e)', () => {
   let app
+  const userService = {
+    charge: () => PointResult,
+    getPoint: () => PointResult,
+  }
+  const productService = {
+    getDetail: () => ProductDetailResult,
+    getRankedProducts: () => ProductsResult,
+  }
+  const payService = {
+    pay: () => SimpleResult,
+  }
+  const orderService = {
+    order: () => SimpleResult,
+  }
+
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile()
+    })
+      .overrideProvider(UserService)
+      .useValue(userService)
+      .overrideProvider(ProductService)
+      .useValue(productService)
+      .overrideProvider(PayService)
+      .useValue(payService)
+      .overrideProvider(OrderService)
+      .useValue(orderService)
+      .compile()
 
     app = moduleFixture.createNestApplication()
     await app.init()
@@ -18,7 +52,7 @@ describe('AppController (e2e)', () => {
     for (let i = 0; i < 10; i++) {
       requests.push(
         request(app.getHttpServer())
-          .patch('/mall/user/:id/charge')
+          .patch('/user/charge/:id')
           .send({ id: '1', amount: 10 - i })
           .expect(200),
       )
