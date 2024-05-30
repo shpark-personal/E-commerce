c1a2f13f13345b76c500c4385861948496be535e
박소희 <shpark-personal>
2024-05-30 18:51:26 +0900

    feat : user 관련 내용 수정

--- src/mall/models/Enums.ts
+++ src/mall/models/Enums.ts
@@ -2,6 +2,8 @@ export enum Errorcode {
   Success = 'Success',
   InvalidRequest = 'Invalid Request',
   InvalidAmount = 'Invalid Amount',
+  UnknownUser = 'Unknown User',
   LackOfPoint = 'Lack of Point',
   OutOfStock = 'Out of Stock',
+  UnknownError = 'Unknown Error',
 }
