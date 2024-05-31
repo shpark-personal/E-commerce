f0e86134a0a5b673ceec20b5c7326d47a8ad0cbd
박소희 <shpark-personal>
2024-05-31 11:08:17 +0900

    test : payService test 수정

--- src/mall/service/pay.service.spec.ts
+++ src/mall/service/pay.service.spec.ts
@@ -1,69 +1,92 @@
-import { TestingModule, Test } from '@nestjs/testing'
+import { todo } from 'node:test'
 import {
-  IUSER_REPOSITORY,
-  IPRODUCT_REPOSITORY,
-  ISTOCK_REPOSITORY,
-  IORDER_REPOSITORY,
+  IOrderRepository,
+  IProductRepository,
+  IStockRepository,
+  IUserRepository,
 } from '../repository/mall.interface'
-import { TestRepository } from '../repository/test.repository'
 import { PayService } from './pay.service'
-// import { Errorcode } from '../models/Enums'
+import { OrderEntity } from '../models/Entities'
+import { Errorcode } from '../models/Enums'
+import { PointResult } from '../models/Result'
 
 describe('PayService', () => {
-  let service: PayService
-  let repo: TestRepository
+  let payService: PayService
+  let userRepositoryMock: IUserRepository
+  let productRepositoryMock: IProductRepository
+  let stockRepositoryMock: IStockRepository
+  let orderRepositoryMock: IOrderRepository
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        PayService,
-        {
-          provide: IUSER_REPOSITORY,
-          useClass: TestRepository,
-        },
-        {
-          provide: IPRODUCT_REPOSITORY,
-          useClass: TestRepository,
-        },
-        {
-          provide: ISTOCK_REPOSITORY,
-          useClass: TestRepository,
-        },
-        {
-          provide: IORDER_REPOSITORY,
-          useClass: TestRepository,
-        },
-        TestRepository,
-      ],
-    }).compile()
-
-    service = module.get<PayService>(PayService)
-    repo = module.get<TestRepository>(TestRepository)
-  })
-
-  it('should be defined', () => {
-    expect(service).toBeDefined()
-    expect(repo).toBeDefined()
+  beforeEach(() => {
+    userRepositoryMock = {
+      charge: jest.fn(),
+      get: jest.fn(),
+      use: jest.fn(),
+    }
+    productRepositoryMock = {
+      getProduct: jest.fn(),
+      updateSales: jest.fn(),
+      getSales: jest.fn(),
+    }
+    stockRepositoryMock = {
+      getStock: jest.fn(),
+      enoughStock: jest.fn(),
+      keepStock: jest.fn(),
+      restoreStock: jest.fn(),
+      depleteStock: jest.fn(),
+    }
+    orderRepositoryMock = {
+      createOrder: jest.fn(),
+      deleteOrder: jest.fn(),
+      createPayment: jest.fn(),
+      getOrder: jest.fn(),
+    }
+    payService = new PayService(
+      userRepositoryMock,
+      productRepositoryMock,
+      stockRepositoryMock,
+      orderRepositoryMock,
+    )
   })
 
   describe('pay', () => {
-    const date = new Date()
-    beforeEach(async () => {
-      await repo.insertSeedOrders(date)
+    it('success to pay', async () => {
+      const userId = 'user123'
+      const date = new Date()
+      const orderForm: OrderEntity = {
+        id: `${date}`,
+        userId: userId,
+        products: [],
+        payment: 50,
+        createTime: date,
+      }
+      orderForm.products = [
+        { id: 1, quantity: 1, order: orderForm },
+        { id: 2, quantity: 1, order: orderForm },
+      ]
+      const pointResult: PointResult = {
+        errorcode: Errorcode.Success,
+        point: 10,
+      }
+
+      jest.spyOn(orderRepositoryMock, 'getOrder').mockResolvedValue(orderForm)
+      jest.spyOn(userRepositoryMock, 'use').mockResolvedValue(pointResult)
+      jest.spyOn(stockRepositoryMock, 'depleteStock')
+      jest.spyOn(orderRepositoryMock, 'createPayment')
+      jest.spyOn(productRepositoryMock, 'updateSales')
+
+      const result = await payService.pay(userId, orderForm.id)
+      expect(result).toEqual({ errorcode: Errorcode.Success })
     })
-
-    // it('success', async () => {
-    //   const result = await service.pay('userA', `${date}`)
-    //   expect(result.errorcode).toEqual(Errorcode.Success)
-    // })
-
-    // it('fail : lack of point', async () => {
-    //   // 임의로 현재 point를 모두 사용함
-    //   const user = await repo.get('userA')
-    //   await repo.use('userA', user.point)
-
-    //   const result = await service.pay('userA', `${date}`)
-    //   expect(result.errorcode).toEqual(Errorcode.LackOfPoint)
-    // })
   })
+
+  // 재고 복구 여부, order 삭제 확인
+  todo('valid id check')
+  todo('orderForm이 없을 때')
+  todo('point use 실패했을 때')
+
+  // db관련 실패
+  todo('depleteStock 실패')
+  todo('payment create 실패')
+  todo('updateSale 실패')
 })
