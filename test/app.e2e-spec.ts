98a9d2ebab076cb628ae6eb5f67420eab27c9ab1
박소희 <shpark-personal>
2024-05-31 16:26:29 +0900

    test : e2e test 작성

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,65 +1,64 @@
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
 import { Test, TestingModule } from '@nestjs/testing'
-import { UserService } from '../src/mall/service/user.service'
-import {
-  PointResult,
-  ProductDetailResult,
-  ProductsResult,
-  SimpleResult,
-} from '../src/mall/models/Result'
-import { ProductService } from '../src/mall/service/product.service'
-import { OrderService } from '../src/mall/service/order.service'
-import { PayService } from '../src/mall/service/pay.service'
+import { todo } from 'node:test'
+import { HttpStatus } from '@nestjs/common'
+import { User } from '../src/mall/models/Entities'
+import { Repository } from 'typeorm'
 
 describe('AppController (e2e)', () => {
   let app
-  const userService = {
-    charge: () => PointResult,
-    getPoint: () => PointResult,
-  }
-  const productService = {
-    getDetail: () => ProductDetailResult,
-    getRankedProducts: () => ProductsResult,
-  }
-  const payService = {
-    pay: () => SimpleResult,
-  }
-  const orderService = {
-    order: () => SimpleResult,
-  }
+  let userRepository: Repository<User>
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })
-      .overrideProvider(UserService)
-      .useValue(userService)
-      .overrideProvider(ProductService)
-      .useValue(productService)
-      .overrideProvider(PayService)
-      .useValue(payService)
-      .overrideProvider(OrderService)
-      .useValue(orderService)
-      .compile()
+    }).compile()
 
     app = moduleFixture.createNestApplication()
     await app.init()
+
+    userRepository = moduleFixture.get('UserRepository')
   })
 
-  it('should concurrently send requests', async () => {
-    const requests = []
-    for (let i = 0; i < 10; i++) {
-      requests.push(
-        request(app.getHttpServer())
-          .patch('/user/charge/:id')
-          .send({ id: '1', amount: 10 - i })
-          .expect(200),
-      )
-    }
-
-    await Promise.all(requests)
+  beforeEach(async () => {
+    await userRepository.clear()
   })
+
+  it('charge point', async () => {
+    const id = 'John'
+    const response = await request(app.getHttpServer())
+      .patch(`/user/charge/${id}`)
+      .send({ amount: 10 })
+      .expect(HttpStatus.OK)
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({ errorcode: 'Success', point: 10 })
+
+    const updatedUser = await userRepository.findOne({ where: { id: id } })
+    expect(updatedUser).toBeDefined()
+    expect(updatedUser.point).toEqual(10)
+  })
+
+  todo('get point')
+  todo('search product')
+  todo('search ranked')
+  todo('order')
+  todo('pay')
+
+  // it('should concurrently send requests', async () => {
+  //   const requests = []
+  //   for (let i = 0; i < 10; i++) {
+  //     requests.push(
+  //       request(app.getHttpServer())
+  //         .patch('/user/charge/:id')
+  //         .send({ id: '1', amount: 10 - i })
+  //         .expect(200),
+  //     )
+  //   }
+
+  //   await Promise.all(requests)
+  // })
 
   afterAll(async () => {
     await app.close()
