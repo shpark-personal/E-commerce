252b57c19b232ffb041f19dbca0723b0a90f91de
박소희 <shpark-personal>
2024-06-14 18:53:39 +0900

    test : 1차 구현

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -49,19 +49,40 @@ describe('AppController (e2e)', () => {
   todo('order')
   todo('pay')
 
-  // it('should concurrently send requests', async () => {
-  //   const requests = []
-  //   for (let i = 0; i < 10; i++) {
-  //     requests.push(
-  //       request(app.getHttpServer())
-  //         .patch('/user/charge/:id')
-  //         .send({ id: '1', amount: 10 - i })
-  //         .expect(200),
-  //     )
-  //   }
+  it('should concurrently send requests', async () => {
+    const id = 1
+    const requests = []
 
-  //   await Promise.all(requests)
-  // })
+    // 요청 배열 생성
+    for (let i = 0; i < 10; i++) {
+      requests.push(
+        request(app.getHttpServer())
+          .patch(`/mall/user/charge/${id}`)
+          .send({ amount: 10 - i })
+          .expect(200),
+      )
+    }
+
+    // 모든 요청을 동시에 실행
+    const responses = await Promise.all(requests)
+
+    // 각 응답 검사
+    responses.forEach((response, index) => {
+      expect(response.status).toBe(200)
+      expect(response.body).toEqual({ errorcode: 'Success', point: 10 - index })
+    })
+
+    // 최종적으로 업데이트된 유저 데이터 확인
+    const updatedUser = await userRepository.findOne({ where: { id: `${id}` } })
+    expect(updatedUser).toBeDefined()
+
+    // 총 포인트 계산
+    const totalPoints = responses.reduce(
+      (acc, _, index) => acc + (10 - index),
+      0,
+    )
+    expect(updatedUser.point).toEqual(totalPoints)
+  })
 
   afterAll(async () => {
     await app.close()
